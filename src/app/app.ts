b776a4cad2611e442eb75ae0100960808f129abb
rashmiprivate <rajput.rashmi0703@gmail.com>
2026-09-06 17:34:14 +0530

    performance

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoaderComponent } from './shared/components/loader/loader.component';
 import { HttpLoadingService } from './core/services/http-loading.service';
 import { ToastComponent } from './shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoaderComponent, ToastComponent],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
